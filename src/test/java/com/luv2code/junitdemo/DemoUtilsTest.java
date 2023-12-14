f21a445aa748eb97c73327d16dec1a5ceaa85ec5
MPAux <mikepoweraux@gmail.com>
2023-12-14 21:12:45 +0100

    added until video no 12

--- src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -1,9 +1,22 @@
 package com.luv2code.junitdemo;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
+//@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+//@DisplayNameGeneration(DisplayNameGenerator.Simple.class)
+//@DisplayNameGeneration(DisplayNameGenerator.IndicativeSentences.class)
 class DemoUtilsTest {
 
     DemoUtils demoUtils;
@@ -11,9 +24,29 @@ class DemoUtilsTest {
     @BeforeEach
     void setupBeforeEach() {
         demoUtils = new DemoUtils();
-        System.out.println("@BeforeEach executes before the execution of each test method");
     }
 
+    @Test
+   // @DisplayName("Equals and Not Equals")
+    void testEqualsAndNotEquals() {
+
+        assertEquals(6, demoUtils.add(2,4), "2+4 must be 6");
+        assertNotEquals(6, demoUtils.add(1,9), "1+9 must not be 6");
+
+    }
+
+    @Test
+  //  @DisplayName("Null and Not Null")
+    void testNullAndNotNull() {
+
+        String str1 = null;
+        String str2 = "luv2code";
+
+        assertNull(demoUtils.checkNull(str1), "Object should be null");
+        assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
+
+    }
+/*
     @AfterEach
     void tearDownAfterEach() {
         System.out.println("Running @AfterEach");
@@ -29,25 +62,5 @@ class DemoUtilsTest {
     static void tearDownAfterAll() {
         System.out.println("@AfterAll executes only once AFTER all test methods.");
     }
-
-    @Test
-    void testEqualsAndNotEquals() {
-        System.out.println("Running test: testEqualsAndNotEquals");
-
-        assertEquals(6, demoUtils.add(2,4), "2+4 must be 6");
-        assertNotEquals(6, demoUtils.add(1,9), "1+9 must not be 6");
-
-    }
-
-    @Test
-    void testNullAndNotNull() {
-        System.out.println("Running test: testNullAndNotNull");
-        String str1 = null;
-        String str2 = "luv2code";
-
-        assertNull(demoUtils.checkNull(str1), "Object should be null");
-        assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
-
-    }
-
+*/
 }

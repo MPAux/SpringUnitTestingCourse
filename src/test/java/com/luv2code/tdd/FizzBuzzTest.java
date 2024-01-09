70d4e4b571bcb5f050ac25a1d26bd71ff6cefbed
MPAux <mikepoweraux@gmail.com>
2024-01-09 20:42:54 +0100

    added until video 43 Every FizzBuzz exercise including csv test files  and main app.

--- src/test/java/com/luv2code/tdd/FizzBuzzTest.java
+++ src/test/java/com/luv2code/tdd/FizzBuzzTest.java
@@ -53,12 +53,28 @@ class FizzBuzzTest {
         assertEquals(expected, FizzBuzz.compute(1), "Should return 1");
     }
 
-    // Testing with small data file
+    // Testing with data files
     @DisplayName("Testing with small data file")
     @ParameterizedTest(name = "value={0}, expected={1}")
-    @CsvFileSource(files = "small-test-data.csv")
+    @CsvFileSource(resources = "/small-test-data.csv")
     @Order(5)
     void testSmallDataFile(int value, String expected) {
+        assertEquals(expected, FizzBuzz.compute(value), "Should return " + expected);
+    }
+
+    @DisplayName("Testing with medium data file")
+    @ParameterizedTest(name = "value={0}, expected={1}")
+    @CsvFileSource(resources = "/medium-test-data.csv")
+    @Order(6)
+    void testMediumDataFile(int value, String expected) {
+        assertEquals(expected, FizzBuzz.compute(value), "Should return " + expected);
+    }
+
+    @DisplayName("Testing with large data file")
+    @ParameterizedTest(name = "value={0}, expected={1}")
+    @CsvFileSource(resources = "/large-test-data.csv")
+    @Order(6)
+    void testLargeDataFile(int value, String expected) {
         assertEquals(expected, FizzBuzz.compute(value), "Should return " + expected);
     }
 }

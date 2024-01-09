0d4893f73a7b407c6c914e880cc0b7a82b33cc4c
MPAux <mikepoweraux@gmail.com>
2024-01-09 20:18:41 +0100

    added CSV file

--- src/test/java/com/luv2code/tdd/FizzBuzzTest.java
+++ src/test/java/com/luv2code/tdd/FizzBuzzTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -48,5 +51,14 @@ class FizzBuzzTest {
     void testForNotDivisibleBy3And5() {
         String expected = "1";
         assertEquals(expected, FizzBuzz.compute(1), "Should return 1");
+    }
+
+    // Testing with small data file
+    @DisplayName("Testing with small data file")
+    @ParameterizedTest(name = "value={0}, expected={1}")
+    @CsvFileSource(files = "small-test-data.csv")
+    @Order(5)
+    void testSmallDataFile(int value, String expected) {
+        assertEquals(expected, FizzBuzz.compute(value), "Should return " + expected);
     }
 }
